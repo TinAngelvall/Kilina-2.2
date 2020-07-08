66a71f7f33efec3930e55f7273788a31c8d164b8
Valentina Kilina <angelvall@bk.ru>
2020-07-08 14:24:31 +0500

    Added colors and strings

--- app/src/main/java/com/example/kilina_22/MainActivity.java
+++ app/src/main/java/com/example/kilina_22/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.kilina_22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAlong;
+    private Button buttonBack;
+    int number = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        final TextView output = (TextView) findViewById(R.id.output);
+
+        method_random();
+        String address_image = getString(R.string.output, number);
+        output.setText(address_image);
+
+        buttonAlong = findViewById(R.id.btn_along);
+        buttonBack = findViewById(R.id.btn_back);
+
+        buttonAlong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                method_random();
+                String address_image = getString(R.string.output, number);
+                output.setText(address_image);
+            }
+        });
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void method_random() {
+
+        Random rand = new Random();
+        number = rand.nextInt(101);
+        int result = number;
     }
 }
